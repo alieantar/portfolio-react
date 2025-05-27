4459fbb5a2cf76338c7e762e8132ff9dd8465b8c
Alie Antar <alieantar2795@outlook.com>
2025-05-27 19:22:35 -0400

    Added responsiveness for more devices. Tablets in portrait mode are now supported. Missed creating responsiveness for large mobile devices so will have to visit that after this commit.

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,11 +2,10 @@ import {content} from './objects.js'
 
 function Content() {
     return (
-        <>
-            <p className='about-me-text' key={content[0].objectId}>
-                {content[0].contentDisplay}
-            </p>
-        </>
+
+        <p className='about-me-text' key={content[0].objectId}>
+            {content[0].contentDisplay}
+        </p>
     )
 }
 
